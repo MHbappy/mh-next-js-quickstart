7f0c600d79233b3121df461f058a9bbc466d0887
mhbappy <mhbappy18@gmail.com>
2026-01-11 16:31:02 +0600

    added

--- src/lib/api/passport.service.ts
+++ src/lib/api/passport.service.ts
@@ -1,4 +1,24 @@
+import {
+  CreatePassportDto,
+  PassportListResponse,
+  PassportSingleResponse
+} from '@/types/passport';
+import { apiClient } from './client';
+
 export class PassportService {
-  // static async createPassport(data : CreatePassportDto){
-  // }
+  static async createPassport(
+    data: CreatePassportDto
+  ): Promise<PassportSingleResponse> {
+    const response = await apiClient.post<PassportSingleResponse>(
+      '/passport-info',
+      data
+    );
+    return response.data;
+  }
+
+  static async getAllPassport(): Promise<PassportListResponse> {
+    const response =
+      await apiClient.get<PassportListResponse>('/passport-info');
+    return response.data;
+  }
 }
